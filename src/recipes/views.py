389cab143803bee805f9f39620e1273a648ca28c
tscholl44 <tscholl4444@gmail.com>
2025-09-05 15:39:43 -0400

    Update views.py

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Recipe
 
 # Create your views here.
-def home(request):
+def recipes_home(request):
    return render(request, 'recipes/recipes_home.html')
+
+class RecipesListView(ListView):
+   model = Recipe
+   template_name = "recipes/recipes_list.html"
+   context_object_name = "recipes"
+
+
+class RecipesDetailView(DetailView):
+   model = Recipe
+   template_name = "recipes/recipes_detail.html"
+   context_object_name = "recipe"
