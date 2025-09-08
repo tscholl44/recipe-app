aa85679c0ac9de2a5893641fd72d017f8d404744
tscholl44 <tscholl4444@gmail.com>
2025-09-08 14:20:57 -0400

    Update urls.py

--- src/recipe_project/urls.py
+++ src/recipe_project/urls.py
@@ -19,10 +19,16 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+
+from .views import login_view, logout_view, logout_success  
+
  
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("recipes.urls"))
+    path("", include("recipes.urls")),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
+    path("logout/success/", logout_success, name="logout_success"),  # Add this line
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
