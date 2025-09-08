614b82df64d21e2dc074984f2ff1479b177611e8
tscholl44 <tscholl4444@gmail.com>
2025-09-08 14:21:07 -0400

    Update views.py

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -2,17 +2,20 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Recipe
 
+#to protect class-based view
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
 def recipes_home(request):
    return render(request, 'recipes/recipes_home.html')
 
-class RecipesListView(ListView):
+class RecipesListView(LoginRequiredMixin, ListView):
    model = Recipe
    template_name = "recipes/recipes_list.html"
    context_object_name = "recipes"
 
 
-class RecipesDetailView(DetailView):
+class RecipesDetailView(LoginRequiredMixin, DetailView):
    model = Recipe
    template_name = "recipes/recipes_detail.html"
    context_object_name = "recipe"
