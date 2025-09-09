a051cc498e69d6d1ea3a2ad89b10ced0b7939b4a
tscholl44 <tscholl4444@gmail.com>
2025-09-09 16:53:12 -0400

    Update urls.py

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import recipes_home, RecipesListView, RecipesDetailView
+from .views import recipes_home, RecipesListView, RecipesDetailView, recipes_list
 
 app_name = 'recipes' 
 
 urlpatterns = [
    path('', recipes_home, name='recipes_home'),
-   path("recipes/", RecipesListView.as_view(), name="recipes_list"),
+   path("recipes/", recipes_list, name="recipes_list"),  # Changed to function-based view
    path("recipes/<pk>", RecipesDetailView.as_view(), name="recipes_detail"),
 ]
